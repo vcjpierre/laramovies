8a15c7b236e0bc8b8ce9aeb79c55ed067c681d44
Jean Pierre Velez <vcjpierre@gmail.com>
2020-04-09 13:25:30 -0400

    Update index function

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -34,7 +34,7 @@ class MoviesController extends Controller
             $genres
         );
 
-        return view('index', $viewModel);
+        return view('movies.index', $viewModel);
     }
 
     /**
@@ -72,7 +72,7 @@ class MoviesController extends Controller
 
         $viewModel = new MovieViewModel($movie);
 
-        return view('show', $viewModel);
+        return view('movies.show', $viewModel);
     }
 
     /**
