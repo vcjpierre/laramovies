adbac796adcc0dc010e0f55ea433f59cf8b8baea
Jean Pierre Velez <vcjpierre@gmail.com>
2020-04-09 13:24:25 -0400

    Add actores routes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Movies
 Route::get('/', 'MoviesController@index')->name('movies.index');
 Route::get('/movies/{movie}', 'MoviesController@show')->name('movies.show');
+
+// Actors
+Route::get('/actors', 'ActorsController@index')->name('actors.index');
+Route::get('/actors/page/{page?}', 'ActorsController@index');
+Route::get('/actors/{actor}', 'ActorsController@show')->name('actors.show');
