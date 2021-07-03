32798e031988083ef3c1706477cf857fb91888b7
Jean Pierre Velez <vcjpierre@gmail.com>
2021-07-02 22:05:44 -0400

    Fix image

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -32,7 +32,7 @@
                 <livewire:search-dropdown>
                 <div class="md:ml-4 mt-3 md:mt-0">
                     <a href="#">
-                        <img src="/img/avatar.jpg" alt="avatar" class="rounded-full w-8 h-8">
+                        <img src="{{ url('img/avatar.jpg') }}" alt="avatar" class="rounded-full w-8 h-8">
                     </a>
                 </div>
             </div>
